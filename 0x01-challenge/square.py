9cb945400d01d87e7285d02afd4b90edd70c5a97
Camilo Barreiro <1845@holbertonschool.com>
2021-02-20 14:23:24 -0500

    Add doctstring, change name class, change name parameter and correction  lin 18

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
+""" Fix my code "Square" """
 
-class square():
-    
+
+class Square():
+    ''' Class Square'''
     width = 0
     height = 0
 
     
     def __init__(self, *args, **kwargs):
+        """Constructor"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
+        """Perimeter of the Square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """Print size of the Square"""
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
-    s = square(width=12, height=9)
+    """Function main"""
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
